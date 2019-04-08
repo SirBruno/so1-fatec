6dd5a2f685dffc62134c9c9ab30370b74f5400c9
SirBruno <bruno.webjr@gmail.com>
2019-04-08 13:17:18 -0300

    ...

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("this is a test");
+console.log("Isso é um teste...");
+
+// requer o pacote "readline-sync"
+// tal pacote serve pra ler entrada do usuário
+var readlineSync = require('readline-sync');
+ 
+// Ler entrada do usuário
+var nome = readlineSync.question('Qual o seu nome? ');
+// mostrar resultado
+console.log('Oi ' + nome + '!');
+
+var comida = readlineSync.question('Qual sua comida favorita? ');
+console.log('Oh, ' + nome + ' ama ' + comida + '!');
